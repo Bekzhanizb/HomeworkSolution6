5545add4f24cedb29899ae6ce49c545b4a26ef76
Bekezhan <bekzhanizbasar228@gmail.com>
2025-04-15 00:39:53 +0500

    Bonus task: Added Logger and its methods

--- src/supportRequestHandler/FAQBotHandler.java
+++ src/supportRequestHandler/FAQBotHandler.java
@@ -1,11 +1,14 @@
 package supportRequestHandler;
 
+import supportRequestHandler.logger.Logger;
+
 public class FAQBotHandler extends SupportHandler {
     @Override
     public void handle(String issue) {
         if (issue.equals("password_reset")) {
             System.out.println("[FAQBot] Processed the request: " + issue);
         } else if (next != null) {
+            Logger.log("FAQBot passes the request on...");
             next.handle(issue);
         } else {
             System.out.println("[FAQBot] Can't process: " + issue);
--- src/supportRequestHandler/JuniorSupportHandler.java
+++ src/supportRequestHandler/JuniorSupportHandler.java
@@ -1,11 +1,14 @@
 package supportRequestHandler;
 
+import supportRequestHandler.logger.Logger;
+
 public class JuniorSupportHandler extends SupportHandler {
     @Override
     public void handle(String issue) {
         if (issue.equals("refund_request") || issue.equals("billing_issue")) {
             System.out.println("[JuniorSupport] Processed the request: " + issue);
         } else if (next != null) {
+            Logger.log("FAQBot passes the request on...");
             next.handle(issue);
         } else {
             System.out.println("[JuniorSupport] Can't process: " + issue);
